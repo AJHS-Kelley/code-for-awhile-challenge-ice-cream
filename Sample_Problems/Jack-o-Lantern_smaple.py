a3f38c665271f27fc522876c9fb8ac42b5887e1b
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2024-03-01 14:28:06 -0500

    456789

---
+++ Sample_Problems/Jack-o-Lantern_smaple.py
@@ -0,0 +1,3 @@
+Designs = input("Please type the number of design options for the eyes, nose, and mouth, each seperated by a space.\n").split()
+possibilities = int(Designs[0]) * int(Designs[1]) * int(Designs[2])
+print(possibilities)
